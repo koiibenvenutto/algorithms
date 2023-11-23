988bd989f78bfbfa343da5f3f4c4dcaf724ee638
koiibenvenutto <58677698+koiibenvenutto@users.noreply.github.com>
2023-11-22 23:48:04 -0800

    here we go

--- euclideanDistance2D.js
+++ euclideanDistance2D.js
@@ -1,15 +1,21 @@
 // Step 1: sort the point by their x axis
 
 const points = [
-  [-4, 6],
-  [2, -8],
-  [1, 0],
-  [9, 8],
+  [-2, -8],
+  [100, 0],
+  [4, 90],
+  [53, 4],
+  [20, 24],
+  [57, 2],
+  [1, 2],
+  [1, 3],
 ];
 
 points.sort((a, b) => {
-  return points[a] - points[b];
+  return a[0] - b[0];
 });
+
+console.log(points);
 
 function compare(left, right) {
   if (left < right) {
@@ -20,21 +26,40 @@ function compare(left, right) {
 }
 
 function findShortestDistance(array) {
-  // return the shortest distance in the passed array in the form of a number, for example: 2.56
+  if (array.length < 2) {
+    return Infinity;
+  }
+
+  let distance = Infinity;
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 1; j < array.length; j++) {
+      if (i === j && j < array.length - 1) {
+        j++;
+      }
+      let result = Math.sqrt(
+        (array[i][0] - array[j][0]) ** 2 + (array[i][1] - array[j][1]) ** 2
+      );
+
+      if (result < distance) {
+        distance = result;
+      }
+    }
+  }
+
+  return distance;
 }
 
-function divideUntilSmallThenShortest(points) {
+function shortest(points) {
   if (points.length <= 3) {
     return findShortestDistance(points);
   }
   const middle = Math.floor(points.length / 2);
-  const left = points.splice(0, middle);
-  const right = points;
+  const left = points.slice(0, middle);
+  const right = points.slice(middle);
 
-  return compare(
-    divideUntilSmallThenShortest(left),
-    divideUntilSmallThenShortest(right)
-  );
+  return compare(shortest(left), shortest(right));
 }
 
-divide(points);
+console.log(shortest(points));
+
+// Okay so since I can't think of a reason why I would need to make slabs instead of just using the columns that I have used already, I'm going to try my method.
