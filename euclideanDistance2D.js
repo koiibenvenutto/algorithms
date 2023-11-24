88efe8d37111691d711634f512a4832d1ed3c1b8
koiibenvenutto <58677698+koiibenvenutto@users.noreply.github.com>
2023-11-23 20:27:52 -0800

    okie

--- euclideanDistance2D.js
+++ euclideanDistance2D.js
@@ -52,6 +52,10 @@ function findShortestDistance(array) {
   return distance;
 }
 
+function eucDist(a, b) {
+  return Math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2);
+}
+
 function split(points) {
   if (points.length <= 3) {
     columns.push(points);
