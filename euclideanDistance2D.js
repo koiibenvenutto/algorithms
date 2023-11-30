b9ca58fdf658f647b3f06184dfdb599126437d85
koiibenvenutto <58677698+koiibenvenutto@users.noreply.github.com>
2023-11-30 15:23:07 -0800

    start

--- euclideanDistance2D.js
+++ euclideanDistance2D.js
@@ -16,16 +16,8 @@ points.sort((a, b) => {
   return a[0] - b[0];
 });
 
-points.sort((a, b) => {
-  return a[0] - b[0];
-});
-
-function compare(left, right) {
-  if (left < right) {
-    return left;
-  } else {
-    return right;
-  }
+function eucDist(a, b) {
+  return Math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2);
 }
 
 function findShortestDistance(array) {
@@ -39,9 +31,7 @@ function findShortestDistance(array) {
       if (i === j && j < array.length - 1) {
         j++;
       }
-      let result = Math.sqrt(
-        (array[i][0] - array[j][0]) ** 2 + (array[i][1] - array[j][1]) ** 2
-      );
+      let result = eucDist(array[i], array[j]);
 
       if (result < distance) {
         distance = result;
@@ -52,99 +42,178 @@ function findShortestDistance(array) {
   return distance;
 }
 
-function eucDist(a, b) {
-  return Math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2);
-}
-
-function split(points) {
-  if (points.length <= 3) {
-    columns.push(points);
-    return findShortestDistance(points);
-  }
-  const middle = Math.floor(points.length / 2);
-  const left = points.slice(0, middle);
-  const right = points.slice(middle);
-
-  return compare(split(left), split(right));
-}
-
-const columns = [];
-
 let d = split(points);
 
-//
-//
-//
-
-for (col = 0; col < columns.length - 1; col++) {
-  let rightMostOfLeft = columns[col][columns[col].length - 1];
-  let rightMostOfLeftX = rightMostOfLeft[0];
-
-  let divide = points.indexOf(rightMostOfLeft) + 1;
-
-  let left = points.slice(0, divide);
-  let right = points.slice(divide);
-
-  let searchBoundaries = [rightMostOfLeftX - d, rightMostOfLeftX + d];
-  console.log(searchBoundaries);
-  const leftCandidates = [];
-  for (let i = 0; i < left.length; i++) {
-    if (
-      left[i][0] >= searchBoundaries[0] &&
-      left[i][0] <= searchBoundaries[1]
+function shortestAcrossColumns(points) {
+  points.sort((a, b) => a[1] - b[1]);
+  let minDistance = d;
+  for (let i = 0; i < points.length; i++) {
+    for (
+      let j = i + 1;
+      j < points.length && points[j][1] - points[i][1] < d;
+      j++
     ) {
-      leftCandidates.push(left[i]);
-    }
-  }
-
-  console.log(leftCandidates);
-
-  const rightCandidates = [];
-  for (let i = 0; i < right.length; i++) {
-    if (
-      right[i][0] >= searchBoundaries[0] &&
-      right[i][0] <= searchBoundaries[1]
-    ) {
-      rightCandidates.push(right[i]);
-    }
-  }
-
-  console.log(rightCandidates);
-
-  //
-  //
-  //
-
-  const leftElects = [];
-  for (let i = 0; i < rightCandidates.length; i++) {
-    let noClosePoint = true;
-    for (let j = 0; j < leftCandidates.length; j++) {
-      if (Math.abs(rightCandidates[i][1] - leftCandidates[j][1]) <= d) {
-        noClosePoint = false;
-        leftElects.push(leftCandidates[j]);
-      }
-    }
-    if (noClosePoint) {
-      rightCandidates.splice(i, 1);
-    }
-  }
-
-  // console.log(rightCandidates);
-
-  for (let i = 0; i < leftElects.length; i++) {
-    for (let j = 0; j < rightCandidates.length; j++) {
-      let result = Math.sqrt(
-        (leftElects[i][0] - rightCandidates[j][0]) ** 2 +
-          (leftElects[i][1] - rightCandidates[j][1]) ** 2
-      );
-      if (result < d) {
-        d = result;
+      let distance = eucDist(points[i], points[j]);
+      if (distance < minDistance) {
+        minDistance = distance;
       }
     }
   }
+  d = minDistance;
+  return minDistance;
+}
 
-  console.log(d);
+//
+//
+//
+function findShortestDistance(points) {
+  if (points.length <= 1) {
+    return;
+  }
+
+  function split(points) {
+    if (points.length <= 3) {
+      columns.push(points);
+      return findShortestDistance(points);
+    }
+    const middle = Math.floor(points.length / 2);
+    const left = points.slice(0, middle);
+    const right = points.slice(middle);
+
+    let shortestAcross = shortestAcrossColumns(points);
+
+    return Math.min(split(left), shortestAcross, split(right));
+  }
+
+  const columns = [];
+  for (col = 0; col < columns.length - 1; col++) {
+    let rightMostOfLeft = columns[col][columns[col].length - 1];
+
+    let divide = points.indexOf(rightMostOfLeft) + 1;
+
+    let searchBoundaries = [rightMostOfLeft[0] - d, rightMostOfLeft[0] + d];
+    console.log(searchBoundaries);
+
+    const candidates = [];
+    for (let i = 0; i < points.length; i++) {
+      if (
+        points[i][0] >= searchBoundaries[0] &&
+        points[i][0] <= searchBoundaries[1]
+      ) {
+        candidates.push(points[i]);
+      }
+    }
+
+    console.log(candidates);
+
+    const elects = [];
+
+    function shortestAcrossColumns(candidates) {
+      candidates.sort((a, b) => a[1] - b[1]);
+      let minDistance = d;
+      for (let i = 0; i < candidates.length; i++) {
+        for (
+          let j = i + 1;
+          j < candidates.length && candidates[j][1] - candidates[i][1] < d;
+          j++
+        ) {
+          let distance = eucDist(candidates[i], candidates[j]);
+          if (distance < minDistance) {
+            minDistance = distance;
+          }
+        }
+      }
+      d = minDistance;
+      return minDistance;
+    }
+
+    shortestAcrossColumns(candidates);
+
+    console.log(d);
+  }
 }
 
 let end = performance.now();
 console.log(`Execution time: ${end - start} milliseconds`);
+
+// Chat GPT's version:
+//
+//
+//
+//
+
+// PERFORMANCE TESTING START
+let startGPT = performance.now();
+
+function calculateDistance(point1, point2) {
+  return Math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2);
+}
+
+function findClosestInPartition(points) {
+  let minDistance = Infinity;
+  for (let i = 0; i < points.length; i++) {
+    for (let j = i + 1; j < points.length; j++) {
+      let distance = calculateDistance(points[i], points[j]);
+      if (distance < minDistance) {
+        minDistance = distance;
+      }
+    }
+  }
+  return minDistance;
+}
+
+function findClosestAcrossPartitions(points, median, d) {
+  let strip = points.filter((point) => Math.abs(point[0] - median) < d);
+  strip.sort((a, b) => a[1] - b[1]);
+
+  let minDistance = d;
+  for (let i = 0; i < strip.length; i++) {
+    for (
+      let j = i + 1;
+      j < strip.length && strip[j][1] - strip[i][1] < d;
+      j++
+    ) {
+      let distance = calculateDistance(strip[i], strip[j]);
+      if (distance < minDistance) {
+        minDistance = distance;
+      }
+    }
+  }
+  return minDistance;
+}
+
+function closestPairOfPoints(points) {
+  if (points.length < 2) {
+    return Infinity;
+  }
+
+  points.sort((a, b) => a[0] - b[0]);
+
+  function findClosestRecursive(pointsSubset) {
+    console.log(pointsSubset);
+
+    if (pointsSubset.length <= 3) {
+      return findClosestInPartition(pointsSubset);
+    }
+
+    let mid = Math.floor(pointsSubset.length / 2);
+    let left = pointsSubset.slice(0, mid);
+    let right = pointsSubset.slice(mid);
+
+    let dLeft = findClosestRecursive(left);
+    let dRight = findClosestRecursive(right);
+
+    let d = Math.min(dLeft, dRight);
+    let median = pointsSubset[mid][0];
+
+    return Math.min(d, findClosestAcrossPartitions(pointsSubset, median, d));
+  }
+
+  return findClosestRecursive(points);
+}
+
+console.log("Shortest distance: ", closestPairOfPoints(points));
+
+// PERFORMANCE TESTING END
+let endGPT = performance.now();
+console.log(`Execution time: ${endGPT - startGPT} milliseconds`);
