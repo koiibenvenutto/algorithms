eac6fe054dcf58f665bd86daeedcf1014a7a29d9
koiibenvenutto <58677698+koiibenvenutto@users.noreply.github.com>
2023-12-19 15:06:33 -0800

    done with canConstruct

--- canConstruct.js
+++ canConstruct.js
@@ -1,23 +1,35 @@
-const wordBank = ["ab", "abc", "cd", "def", "abcd"];
-const targetStr = "abcdef";
+const wordBank = ["purp", "p", "ur", "le", "purpl"];
+const targetStr = "purple";
 
-const canConstruct = (targetStr, wordBank) => {
-  if (targetStr === "") return true;
+const canConstruct = (targetStr, wordBank, memo = {}) => {
+  if (targetStr in memo) return memo[targetStr];
+  if (targetStr === "") return 1;
 
+  let sum = 0;
   for (let word of wordBank) {
-    if (targetStr.slice(0, word.length) === word) {
-      const suffix = targetStr.slice(word.length);
-      if (canConstruct(suffix, wordBank)) return true;
+    if (targetStr.indexOf(word) === 0) {
+      sum += canConstruct(targetStr.slice(word.length), wordBank, memo);
     }
   }
-
-  return false;
+  memo[targetStr] = sum;
+  console.log(memo);
+  return sum;
 };
 
 console.log(canConstruct(targetStr, wordBank));
+// console.log(
+//   canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeek", [
+//     "e",
+//     "eeee",
+//     "eek",
+//   ])
+// );
+console.log(canConstruct("kitten", ["ki", "it", "i", "en", "k", "ten", "t"]));
 
 // targetStr.slice(targetStr.length - word.length) === word
 
 // NOTE: you can't subtract strings from each other the way you would numbers.
 // console.log(targetStr.slice(0, -wordBank[0].length));
 // console.log(targetStr.slice(wordBank[0].length));
+
+// Now the goal is to return a string that
